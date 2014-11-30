46304db5d1c83fba77e71d4b643f6cd3117dc7a1
Andrew Pickford <AndrewPickford@users.noreply.github.com>
2014-11-30 11:23:21 +0000

    add global config

---
+++ Source/TheWriteStuff/Global.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ASP
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class Global : MonoBehaviour
+    {
+        public static bool Debugging = false;
+        public static Global Instance { get; private set; }
+
+        public void Awake()
+        {
+            if (Instance != null) return;
+
+            foreach (UrlDir.UrlConfig url in GameDatabase.Instance.GetConfigs("THE_WRITE_STUFF"))
+            {
+                if (url.config.HasValue("debug")) Debugging = bool.Parse(url.config.GetValue("debug"));
+            }
+        }
+    }
+}
