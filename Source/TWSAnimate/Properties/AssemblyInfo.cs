67e802082e86741223df24a979cbad2f31e28679
Andrew Pickford <AndrewPickford@users.noreply.github.com>
2016-11-13 13:54:42 +0100

    add customised animation module

--- Source/TWSAnimate/Properties/AssemblyInfo.cs
+++ Source/TWSAnimate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TWSPainter")]
+[assembly: AssemblyTitle("TWSAnimate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TWSPainter")]
+[assembly: AssemblyProduct("TWSAnimate")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0a1c8e66-5832-4786-84fc-ae571aaba2d6")]
+[assembly: Guid("40b9e43c-fe10-49b6-ba58-864a1e48028d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,9 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-
-[assembly: KSPAssembly("TWSPainter", 0, 1)]
-[assembly: KSPAssemblyDependency("TheWriteStuff", 0, 9)]
-[assembly: KSPAssemblyDependency("KIS", 1, 2)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("10.1.0.0")]
---
+++ Source/TWSAnimate/TWSAnimate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ASP
+{
+    public class TWSAnimate : ModuleAnimateGeneric
+    {
+        [KSPEvent(name = "RaiseMaxHeightQuick", guiName = "Raise Max Height ++", active = true, guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, externalToEVAOnly = true)]
+        public void RaiseMaxHeightQuick()
+        {
+            deployPercent += 1f;
+            if (deployPercent > 100f) deployPercent = 100f;
+        }
+
+        [KSPEvent(name = "RaiseMaxHeight", guiName = "Raise Max Height +", active = true, guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, externalToEVAOnly = true)]
+        public void RaiseMaxHeight()
+        {
+            deployPercent += 0.1f;
+            if (deployPercent > 100f) deployPercent = 100f;
+        }
+
+        [KSPEvent(name = "LowerMaxHeight", guiName = "Lower Max Height -", active = true, guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, externalToEVAOnly = true)]
+        public void LowerMaxHeight()
+        {
+            deployPercent -= 0.1f;
+            if (deployPercent < 0f) deployPercent = 0f;
+        }
+
+        [KSPEvent(name = "LowerMaxHeightQuick", guiName = "Lower Max Height --", active = true, guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, externalToEVAOnly = true)]
+        public void LowerMaxHeightQuick()
+        {
+            deployPercent -= 1f;
+            if (deployPercent < 0f) deployPercent = 0f;
+        }
+    }
+}
