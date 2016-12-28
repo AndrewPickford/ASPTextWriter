763df5c4765eeac3f6329cb9c7c1847e03e7fb88
Andrew Pickford <AndrewPickford@users.noreply.github.com>
2016-12-28 19:57:01 +0100

    rename vessel during eva

---
+++ Source/TheWriteStuff/ASPEvaRename.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ASP
+{
+    public class ASPEvaRename : PartModule
+    {
+        [KSPEvent(name = "renameVesselEvent", guiName = "Rename", guiActive = false, guiActiveEditor = false, guiActiveUnfocused = true, externalToEVAOnly = true)]
+        public void renameVesselEvent()
+        {
+            vessel.RenameVessel();
+        }
+    }
+}
